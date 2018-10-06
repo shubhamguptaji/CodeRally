e0610d88677205c1d967b417bb56bd5ec672884d
shubhamguptaji <shubham2604gupta@gmail.com>
2018-10-06 15:08:29 +0530

    fixed issues

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,7 @@
 import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
 import { withStyles } from "@material-ui/core/styles";
 import deepPurple from "@material-ui/core/colors/deepPurple";
 
@@ -13,21 +16,19 @@ const styles = () => ({
   }
 });
 
-const Header = () => (
-  <div>
-    <h1
-      style={{
-        backgroundColor: deepPurple[700],
-        color: "white",
-        padding: "10px",
-        textAlign: "center",
-        fontSize: "60px",
-        marginTop: "0px"
-      }}
-    >
-      Code Rally
-    </h1>
-  </div>
+const Header = ({ classes }) => (
+  <AppBar className={classes.root}>
+    <Toolbar>
+      <Typography variant="title" color="inherit">
+        <a
+          style={{ textDecoration: "none", color: "inherit", padding: "10px" }}
+          href="/"
+        >
+          CodeRally
+        </a>
+      </Typography>
+    </Toolbar>
+  </AppBar>
 );
 
 export default withStyles(styles)(Header);
--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-import Typist from "react-typist";
 import { Button } from "@material-ui/core";
-import { Link } from "react-router-dom";
+import lightBlue from "@material-ui/core/colors/lightBlue";
+import grey from "@material-ui/core/colors/grey";
 
 class LandingPage extends Component {
   render() {
@@ -12,32 +12,33 @@ class LandingPage extends Component {
             marginTop: "40px",
             marginLeft: "50px",
             marginRight: "50px",
-            minHeight: "350px"
+            minHeight: "200px"
           }}
         >
-          <h3 style={{ fontSize: "40px" }}>
-            <div style={{ paddingTop: "10px" }}>
+          <h3 style={{ fontSize: "30px" }}>
+            <div style={{ paddingTop: "10px", textAlign: "center" }}>
               <p>Get experience by building real apps with a real team.</p>
-              <Typist>
+              <p>
                 CodeRally exists so developers who want to boost their skills
                 can get the experience they need to get to the next level. There
                 really is no better qualification than real project experience!
-                <br />
-                <br />
-                Wanna create aur fix real life projects click the button below
-                :)
-              </Typist>
+              </p>
             </div>
           </h3>
         </div>
-        <div style={{ marginLeft: "45%", marginRight: "50%" }}>
-          <Link to="/home">
-            <Button variant="contained" color="primary">
-              <h3 style={{ paddingLeft: "15px", paddingRight: "15px" }}>
-                Home
-              </h3>
-            </Button>
-          </Link>
+        <div
+          style={{
+            width: '100%',
+            display: 'flex',
+            justifyContent: 'center'
+          }}
+        >
+          <Button
+            href="/home"
+            style={{ backgroundColor: grey[200], color: lightBlue[500] }}
+          >
+            Get Started
+          </Button>
         </div>
       </div>
     );
